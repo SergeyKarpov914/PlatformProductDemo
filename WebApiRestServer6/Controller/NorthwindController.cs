787f0b0fba3ab3a3a280360090421eb2aacb0af4
Sergey Karpov <cliovox0409@gmail.com>
2024-11-08 08:10:22 -0500

    expand controller to handle new domain components

--- WebApiRestServer6/Controller/NorthwindController.cs
+++ WebApiRestServer6/Controller/NorthwindController.cs
@@ -2,6 +2,7 @@
 using Clio.Demo.DataPresentation.Elements;
 using Clio.Demo.DataPresentation.ViewModel;
 using Clio.Domain.MultiCrudModel.ViewModel.Element;
+using Clio.Domain.NorthwindModel.Element;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Acsp.Server.WebApiRestServer6.Controller
@@ -16,15 +17,7 @@ namespace Acsp.Server.WebApiRestServer6.Controller
             processor.Inject<NorthwindViewModel>(out _viewModel);
         }
 
-        [Route("api/order")]
-        [HttpGet]
-        public async Task<IActionResult> GetOrders()
-        {
-            IEnumerable<OrderElement> orders = await _viewModel.GetOrders();
-
-            return this.Success(orders);
-        }
-
+        [Route("api/order/")]
         [Route("api/order/{key}")]
         [HttpGet]
         public async Task<IActionResult> GetOrders(string key = null)
@@ -41,6 +34,46 @@ namespace Acsp.Server.WebApiRestServer6.Controller
             IEnumerable<OrderDealElement> deals = await _viewModel.GetDeals(key);
 
             return this.Success(deals);
+        }
+
+        [Route("api/customer/")]
+        [Route("api/customer/{key}")]
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers(string key = null)
+        {
+            IEnumerable<CustomerElement> elements = await _viewModel.GetCustomers(key);
+
+            return this.Success(elements);
+        }
+
+        [Route("api/product/")]
+        [Route("api/product/{key}")]
+        [HttpGet]
+        public async Task<IActionResult> GetProducts(string key = null)
+        {
+            IEnumerable<ProductElement> elements = await _viewModel.GetProducts(key);
+
+            return this.Success(elements);
+        }
+
+        [Route("api/supplier/")]
+        [Route("api/supplier/{key}")]
+        [HttpGet]
+        public async Task<IActionResult> GetSuppliers(string key = null)
+        {
+            IEnumerable<SupplierElement> elements = await _viewModel.GetSuppliers(key);
+
+            return this.Success(elements);
+        }
+
+        [Route("api/employee/")]
+        [Route("api/employee/{key}")]
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees(string key = null)
+        {
+            IEnumerable<EmployeeElement> elements = await _viewModel.GetEmployees(key);
+
+            return this.Success(elements);
         }
     }
 }
